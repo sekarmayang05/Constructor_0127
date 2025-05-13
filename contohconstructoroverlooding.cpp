8a48ca6350514f543bef1aa8b431c6e7b6e9894d
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-13 19:07:50 +0700

    definisi hanya nim

--- contohconstructoroverlooding.cpp
+++ contohconstructoroverlooding.cpp
@@ -7,5 +7,16 @@ private :
 int nim;
 string nama;
 public:
+mahasiswa();
+mahasiswa (int);
+mahasiswa (string);
+mahasiswa (int iNim, string iNama);
+void cetak ();
+};
 
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa (int iNim){
+    nim = iNim; // definisi hanya NIM
 }

63a7e2cefa1eed79e81eb913e67853787e3b0eab
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-13 19:22:12 +0700

    memberi variabel judul

---
+++ this.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class buku{
+    string judul;
+    public:
+        string setget (string jdl)
+        {
+            //nilai parameter 'judul' untuk member variabel 'judul'
+            this ->judul = jdl;
+            //return variabel judul.
+            return this->judul;
+        }
+};

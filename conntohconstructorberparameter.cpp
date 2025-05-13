669c25a8c84b41a8d2b7bae6d778b56a3162175a
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-13 18:52:46 +0700

    membuat class mahasiswa

---
+++ conntohconstructorberparameter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+    public:
+        // Construktor dengan parameter
+        mahasiswa (int nim, string nama);
+};
+
+//

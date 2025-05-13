f97d685366dbe209abd28cb470409fc066c157ae
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-13 19:11:35 +0700

    definisi NIM dan NAMA

--- contohconstructoroverlooding.cpp
+++ contohconstructoroverlooding.cpp
@@ -24,3 +24,7 @@ mahasiswa :: mahasiswa (int iNim){
 mahasiswa :: mahasiswa(string iNama){
     nama = iNama; //definisi hanya Nama
 }
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; //definisi NIM dan NAMA
+    nama = iNama;
+}
